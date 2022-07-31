bc8a4f7095ac3d8bf1d88346f4d87c0a89e2ac32
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2022-07-31 19:43:20 +0800

    Create MakeNo.java

---
+++ 分治/MakeNo.java
@@ -0,0 +1,25 @@
+public class MakeNo {
+    public static int[] makeNo(int size) {
+        if (size == 1) {
+            return new int[] {1};
+        }
+        // size
+        // 一半长达标来
+        // 7 : 4
+        // 8 : 4
+        // [4个奇数] [3个偶数]
+        int halfSize = (size + 1) / 2;
+        int[] base = makeNo(halfSize);
+        // base -> 等长奇数达标来
+        // base -> 等长偶数达标来
+        int[] ans = new int[size];
+        int index = 0;
+        for (; index < halfSize; index++) {
+            ans[index] = base[index] * 2 - 1;
+        }
+        for (int i = 0; index < size; index++, i++) {
+            ans[index] = base[i] * 2;
+        }
+        return ans;
+    }
+}
