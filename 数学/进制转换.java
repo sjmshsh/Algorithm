be4645f84cb147a0a44d2c0d62a71b0bc2cdbca2
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-01-22 20:21:17 +0800

    Update 进制转换.java

--- 数学/进制转换.java
+++ 数学/进制转换.java
@@ -1,3 +1,40 @@
 // 给定一个十进制数M，以及需要转换的进制数N。将十进制数M转化为N进制数
 
 // 思路，这道题的代码非常优雅，要多学习
+import java.util.*;
+
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNextInt()) { 
+            int m = in.nextInt();
+            int n = in.nextInt();
+            if (m == 0) {
+                System.out.println("0");
+                continue;
+            }
+            // 定义一张哈希表
+            String table = "0123456789ABCDEF";
+            // 定义存放结果的字符串
+            String res = "";
+
+            // 首先判断正负
+            boolean flg = true;
+            if (m < 0) {
+                m = 0 - m;
+                flg = false; // 代表是负数
+            }
+            while (m != 0) {
+                res += table.charAt(m % n);
+                m = m / n;
+            }
+            if (flg == false) {
+                res += "-";
+            }
+            StringBuffer sb = new StringBuffer(res);
+            System.out.println(sb.reverse());
+        }
+    }
+}
