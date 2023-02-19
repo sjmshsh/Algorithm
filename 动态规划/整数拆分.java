ed466fc1efed695c93e7bb5bc49b9de6b31f9d69
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-19 13:27:50 +0800

    Create 整数拆分.java

---
+++ 动态规划/整数拆分.java
@@ -0,0 +1,20 @@
+// 给定一个正整数 n ，将其拆分为 k 个 正整数 的和（ k >= 2 ），并使这些整数的乘积最大化。
+
+// 返回 你可以获得的最大乘积 。
+
+// https://leetcode.cn/problems/integer-break/
+
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 2; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * dp[i - j], j * (i - j)));
+            }
+        }
+        return dp[n];
+    }
+}
