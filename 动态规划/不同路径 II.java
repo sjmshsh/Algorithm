e084180ccbdbd76456d0ac527b91ec08a9a475e5
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-19 12:17:29 +0800

    Update 不同路径 II.java

--- 动态规划/不同路径 II.java
+++ 动态规划/不同路径 II.java
@@ -8,6 +8,7 @@
 
 // 链接：https://leetcode.cn/problems/unique-paths-ii
 
+// 方法一，我自己写的，但是不够简洁
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
@@ -38,6 +39,31 @@ class Solution {
                     dp[i][j] = 0;
                 } else {
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+
+
+// 方法二：这个方法的写法很简洁，精悍
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] != 1) {
+                    if (i > 0 && j > 0) {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    } else if (i > 0) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else if (j > 0) {
+                        dp[i][j] = dp[i][j - 1];
+                    }
                 }
             }
         }
