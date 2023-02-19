b7dd84361159cb6258b1bc3fc5811550a1c54c8c
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-19 12:10:39 +0800

    Create 不同路径 II.java

---
+++ 动态规划/不同路径 II.java
@@ -0,0 +1,46 @@
+// 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+
+// 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+
+// 网格中的障碍物和空位置分别用 1 和 0 来表示。
+
+// 链接：https://leetcode.cn/problems/unique-paths-ii
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        dp[0][0] = 1; // (0, 0)的位置有一种方式可以到达
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = dp[i - 1][0];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                dp[0][i] = 0;
+            } else {
+                dp[0][i] = dp[0][i - 1];
+            }
+        }
+        // 边界处理完成
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
