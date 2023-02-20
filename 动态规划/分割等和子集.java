7496b3f2da224eb2b661bcb2ff4395c9685459ac
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-20 21:39:10 +0800

    Update 分割等和子集.java

--- 动态规划/分割等和子集.java
+++ 动态规划/分割等和子集.java
@@ -1,5 +1,7 @@
 // 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
 
+// https://leetcode.cn/problems/partition-equal-subset-sum/
+
 class Solution {
     public boolean canPartition(int[] nums) {
         int N = nums.length;
