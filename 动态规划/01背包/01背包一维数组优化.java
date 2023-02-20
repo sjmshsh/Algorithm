66f5d4efbbe60b3333a315c7a87ecfb3f30d2f51
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-20 21:32:27 +0800

    Create 01背包一维数组优化.java

---
+++ 动态规划/01背包/01背包一维数组优化.java
@@ -0,0 +1,29 @@
+// http://oj.daimayuan.top/course/5/problem/161
+
+public class _01BagOne {
+
+    //n表示有n个物品可以取，m表示有物体的总体积是m
+    private static int n;
+    private static int m;
+    // 体积
+    private static int[] v = new int[1010];
+    // 收益
+    private static int[] w = new int[1010];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            v[i] = sc.nextInt();
+            w[i] = sc.nextInt();
+        }
+        int[] dp = new int[1010];
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= v[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - v[i]] + w[i]);
+            }
+        }
+        System.out.println(dp[m]);
+    }
+}
