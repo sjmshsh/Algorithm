7dbf97bdfe923e09a45489ae60d61116ae85c308
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-27 10:40:24 +0800

    Create 二叉树的前序遍历(迭代写法).java

---
+++ 二叉树/二叉树的前序遍历(迭代写法).java
@@ -0,0 +1,22 @@
+ // 前序 -- 右边先入栈，然后是左边再次入栈
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null){
+                stack.push(node.right);
+            }
+            if (node.left != null){
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+}
