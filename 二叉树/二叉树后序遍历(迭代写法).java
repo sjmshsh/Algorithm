31093766fca6a860b81cc66e4b28c5ef0deeba74
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-02-27 10:52:00 +0800

    Create 二叉树后序遍历(迭代写法).java

---
+++ 二叉树/二叉树后序遍历(迭代写法).java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.left != null){
+                stack.push(node.left);
+            }
+            if (node.right != null){
+                stack.push(node.right);
+            }
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
