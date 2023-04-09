3cbf3db2b504100c5247e336351e0a2f8fed090f
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-04-09 19:59:25 +0800

    Create 不相交的线.java

---
+++ 每日算法打卡/4/9/不相交的线.java
@@ -0,0 +1,18 @@
+class Solution {
+    // 就是最长公共子序列而已
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int N = nums1.length;
+        int M = nums2.length;
+        int[][] dp = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[N][M];
+    }
+}
