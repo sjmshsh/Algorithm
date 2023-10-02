928d0db0e28f3d289d559b15b572c1a16f44fa3a
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-10-02 16:15:47 +0800

    Create merge-two-sorted-lists.cpp

---
+++ 链表/merge-two-sorted-lists.cpp
@@ -0,0 +1,38 @@
+// https://leetcode.cn/problems/merge-two-sorted-lists/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // 定义一个哨兵位节点
+        ListNode* dum = new ListNode(0);
+        // cur是用来遍历的指针
+        ListNode* cur = dum;
+        while (list1 != nullptr && list2 != nullptr) {
+          if (list1->val < list2->val) {
+            cur->next = list1;
+            list1 = list1->next;
+          } else {
+            cur->next = list2;
+            list2 = list2->next;
+          }
+          cur = cur->next;
+        }
+        // 不要忘记了需要把剩下的一截没有遍历完的也加进入
+        if (list1 == nullptr) {
+          cur->next = list2;
+        } else {
+          cur->next = list1;
+        }
+        return dum->next;
+    }
+};
