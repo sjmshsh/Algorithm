42a39674818e67b9c8237e699fa763169d0416aa
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-10-03 21:32:15 +0800

    Create remove-linked-list-elements.cpp

---
+++ 链表/remove-linked-list-elements.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (head == nullptr) {
+            return head;
+        }
+        // 因为删除可能涉及到头节点，所以设置dummy节点，统一操作
+        ListNode* dummy = new ListNode(-1, head);
+        ListNode* pre = dummy;
+        ListNode* cur = head;
+        while (cur != nullptr) {
+            if (cur->val == val) {
+                pre->next = cur->next;
+            } else {
+                pre = cur;
+            }
+            cur = cur->next;
+        }
+        return dummy->next;
+    }
+};
