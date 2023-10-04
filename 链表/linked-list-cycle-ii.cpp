fabcf7d8b2c79187f67954367f571782122e3d19
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-10-04 18:26:36 +0800

    Create linked-list-cycle-ii.cpp

---
+++ 链表/linked-list-cycle-ii.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.cn/problems/linked-list-cycle-ii/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            // 快慢指针相遇了
+            if (fast == slow) {
+                ListNode* index1 = fast;
+                ListNode* index2 = head;
+                while (index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2;
+            }
+        }
+        return nullptr;
+    }
+};
