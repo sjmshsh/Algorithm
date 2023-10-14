aa0f7f15c33b33dcfca04e454839d23df22cf624
无言独上机房 <88866917+sjmshsh@users.noreply.github.com>
2023-10-15 01:10:02 +0800

    Create 01背包压缩.cpp

---
+++ bytedance/01背包压缩.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// 有n个物品可以取, m表示物品的总体积是m
+int n, m;
+int v[1001]; // 体积
+int w[1001]; // 收益
+int dp[1001][1001];
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> v[i] >> w[i];
+	}
+	// 背包压缩
+	for (int i = 1; i <= n; i++) {
+		for (int j = m; j >= v[i]; j--) {
+			dp[j] = max(dp[j], dp[j - v[i]] + w[i]);
+		}
+	}
+	cout << dp[n][m];
+	return 0;
+}
